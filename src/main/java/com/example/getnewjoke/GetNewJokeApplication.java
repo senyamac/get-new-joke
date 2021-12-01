9f8e3dec14aea4217e2ae51c305f0c99f7a4bfef
Arseniy Mayachkin <senya158@gmail.com>
2021-12-01 15:15:58 +0300

    Add project

---
+++ src/main/java/com/example/getnewjoke/GetNewJokeApplication.java
@@ -0,0 +1,13 @@
+package com.example.getnewjoke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetNewJokeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GetNewJokeApplication.class, args);
+  }
+
+}
